a521759ccb35e109325916d1011ad2a843fe2556
davidmgudeman <davidmgudeman@gmail.com>
2017-04-12 11:23:53 -0700

    project06- set up the animations module and made sure app compiled

--- project06-routing-animation/src/app/home/home.component.ts
+++ project06-routing-animation/src/app/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute }    from '@angular/router';
-
+import {trigger, state, animate, style, transition, AnimationEntryMetadata} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
 
